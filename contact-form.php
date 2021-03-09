848432dcbcc02d4b7ae2871feb843a96ce337ba3
joaopcos <joaopcos3@gmail.com>
2021-03-09 14:51:30 -0300

    Script de email incluido

--- contact-form.php
+++ contact-form.php
@@ -2,8 +2,14 @@
     $email = "reservas@villaolivatennis.com";
 	$headers = "MIME-Version: 1.0\n";
 	$headers = "Content-type: text/html; charset=iso-8859-1\n";
-	$headers = "From:". $_POST['email-contato'];
-	$subject = "Mensagem de". $_POST['nome-contato'];
+	$headers = "From: ". $_POST['email-contato'];
+	$subject = "Mensagem de ". $_POST['nome-contato'];
 	$mensagem = $_POST['mensagem-contato'];
 	mail($email, $subject, $mensagem, $headers);
+
 ?>
+
+<script>
+    alert("O email foi enviado, espere pela resposta no email informado!");
+    window.close();
+</script>
